6cdde0e8223c14e25be82ae055c0377ae9c1d87a
isaiahstone21 <isstone21@hotmail.com>
2023-06-02 16:00:00 -0500

    Able to access PSQL database

--- main.py
+++ main.py
@@ -6,7 +6,8 @@ from flask_sqlalchemy import SQLAlchemy
 load_dotenv()
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = ''
+app.config['SQLALCHEMY_DATABASE_URI'] = f"postgresql://{os.getenv('DB_USERNAME')}:{os.getenv('DB_PASSWORD')}@{os.getenv('DB_HOST')}:{os.getenv('DB_PORT')}/{os.getenv('DB_NAME')}"
+db = SQLAlchemy(app)
 
 @app.route('/')
 def index():
