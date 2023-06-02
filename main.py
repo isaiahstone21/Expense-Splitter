0ab48a7d1cc586e0df278d9bd30cebda7e85e039
isaiahstone21 <isstone21@hotmail.com>
2023-06-01 22:56:52 -0500

    Fixed filepaths

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return 'This is the root of the website.'
+    return render_template('index.html', message='This is the root of the website.')
 
-if __name__ == "__main__":
-    app.run(debug=True)
+if __name__ == '__main__':
+    app.run()
