ace9a18e53bab5841e1c7c7c29b35c981d5a3724
Lance <91716701+Lancealot4567@users.noreply.github.com>
2023-06-02 19:56:27 -0500

    Added Functionality For active_screen in navbar

--- main.py
+++ main.py
@@ -9,21 +9,27 @@ app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = f"postgresql://{os.getenv('DB_USERNAME')}:{os.getenv('DB_PASSWORD')}@{os.getenv('DB_HOST')}:{os.getenv('DB_PORT')}/{os.getenv('DB_NAME')}"
 db = SQLAlchemy(app)
 
+#vars
+active_screen = 0
+
 @app.route('/')
 def index():
     return render_template('index.html')
 
 @app.route('/dashboard')
 def dashboard():
-    return render_template('dashboard.html')
+    active_screen = 1
+    return render_template('dashboard.html', active_screen=active_screen)
 
 @app.route('/expenses')
 def expenses():
-    return render_template('expenses.html')
+    active_screen = 2
+    return render_template('expenses.html', active_screen=active_screen)
 
 @app.route('/settings')
 def settings():
-    return render_template('settings.html')
+    active_screen = 3
+    return render_template('settings.html', active_screen=active_screen)
 
 port = os.getenv('PORT')
 
